3bb5a020f19f697868665022c33303f0316bde25
EDU NSBM <edunsbm@gmail.com>
2026-03-31 15:13:43 +0530

    Use elif with and (no nested statements) for 6

--- question6.py
+++ question6.py
@@ -1,4 +1,4 @@
-def main() -> None:
+def main0() -> None:
     while True:
         try:
             number1: float = float(input("Enter number 1: "))
@@ -18,6 +18,24 @@ def main() -> None:
             print(number2)
         else:
             print(number3)
+
+
+def main() -> None:
+    while True:
+        try:
+            number1: float = float(input("Enter number 1"))
+            number2: float = float(input("Enter number 2"))
+            number3: float = float(input("Enter number 3"))
+            break
+        except ValueError:
+            print("Invalid value. Retry")
+
+    if number1 >= number2 and number1 >= number3:
+        print(number1)
+    elif number2 >= number1 and number2 >= number3:
+        print(number2)
+    else:
+        print(number3)
 
 
 if __name__ == "__main__":
